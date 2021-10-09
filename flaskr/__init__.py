4336488ac41f646d57b2b5139a2cd1544356a10f
Jose Ramirez <j.ramirez857@gmail.com>
2021-10-09 14:35:01 -0400

    Added views and blueprints

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -2,6 +2,7 @@ import os
 
 from flask import Flask
 
+
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
@@ -16,7 +17,7 @@ def create_app(test_config=None):
     else:
         # load the test config if passed in
         app.config.from_mapping(test_config)
-    
+
     # ensure the instance folder exists
     try:
         os.makedirs(app.instance_path)
@@ -27,5 +28,11 @@ def create_app(test_config=None):
     @app.route('/hello')
     def hello():
         return 'Hello, World!'
+
+    from . import db
+    db.init_app(app)
+
+    from . import auth
+    app.register_blueprint(auth.bp)
 
     return app
